49b01c40727627d032dc7d2e8229f77ef941bf88
OmshriNT <61307578+OmshriNT@users.noreply.github.com>
2021-01-09 12:27:42 +0530

    commit

--- script.js
+++ script.js
@@ -16,5 +16,5 @@ form.addEventListener('submit', e => {
 
 function validationEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
+    return re.test(String(email).toLowerCase());
 }
